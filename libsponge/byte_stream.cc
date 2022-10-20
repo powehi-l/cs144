cb61706ae54de607b2fa1778beca83a2007d873e
pluto <1119272440@qq.com>
2022-10-19 19:06:43 -0700

    modify the write and pop in byte_stream which should be faster

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -18,39 +18,51 @@ ByteStream::ByteStream(const size_t capacity) {
 }
 
 size_t ByteStream::write(const string &data) {
-    if (data.length() <= (capacity_ - size)) {
-        buffer.append(data);
-        size += data.length();
-        total_in += data.length();
-        return data.length();
-    } else {
-        buffer.append(data.substr(0, capacity_ - size));
-        size_t add = capacity_ - size;
-        size = capacity_;
-        total_in += add;
-        return add;
-    }
+    // if (data.length() <= (capacity_ - size)) {
+    //     buffer.append(data);
+    //     size += data.length();
+    //     total_in += data.length();
+    //     return data.length();
+    // } else {
+    //     buffer.append(data.substr(0, capacity_ - size));
+    //     size_t add = capacity_ - size;
+    //     size = capacity_;
+    //     total_in += add;
+    //     return add;
+    // }
+    int number = min(data.length(), capacity_-size);
+    buffer.append(data.substr(0,number));
+    size += number;
+    total_in += number;
+    return number;
 }
 
 //! \param[in] len bytes will be copied from the output side of the buffer
 string ByteStream::peek_output(const size_t len) const {
-    if (len < size)
-        return buffer.substr(len - 1);
-    else
-        return buffer;
+    // if (len < size)
+    //     return buffer.substr(len - 1);
+    // else
+    //     return buffer;
+
+    return buffer.substr(0, (len<size)?len:size);
 }
 
 //! \param[in] len bytes will be removed from the output side of the buffer
 void ByteStream::pop_output(const size_t len) {
-    if (len < size) {
-        buffer = buffer.substr(len, buffer.length());
-        total_out += len;
-        size -= len;
-    } else {
-        buffer = "";
-        total_out += size;
-        size = 0;
-    }
+    // if (len < size) {
+    //     buffer = buffer.substr(len, buffer.length());
+    //     total_out += len;
+    //     size -= len;
+    // } else {
+    //     buffer = "";
+    //     total_out += size;
+    //     size = 0;
+    // }
+    int number = (len<size) ? len : size;
+    buffer = buffer.substr(number, size-1);
+    total_out += number;
+    size -= number;
+
 }
 
 //! Read (i.e., copy and then pop) the next "len" bytes of the stream
